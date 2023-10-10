be823fb176acbfdb5a521e1ac7bce13e85abde99
Andres Martinez <andres.martinez@habil.mx>
2023-10-10 15:34:34 -0600

    base de sspa y vite para mife

---
+++ src/spa.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOMClient from 'react-dom/client'
+// @ts-ignore
+import singleSpaReact from 'single-spa-react'
+import { cssLifecycle } from 'vite-plugin-single-spa/ex'
+import App from './App'
+
+const lc = singleSpaReact({
+  React,
+  ReactDOMClient,
+  rootComponent: App,
+  errorBoundary(err: any, _info: any, _props: any) {
+    return <div>Error: {err}</div>
+  },
+})
+
+export const bootstrap = [cssLifecycle.bootstrap, lc.bootstrap]
+export const mount = [cssLifecycle.mount, lc.mount]
+export const unmount = [cssLifecycle.unmount, lc.unmount]
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import vitePluginSingleSpa from 'vite-plugin-single-spa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    vitePluginSingleSpa({
+      type: 'mife',
+      serverPort: 9010,
+      spaEntryPoint: 'src/spa.tsx',
+      projectId: 'sspamife',
+    }),
+  ],
+})
